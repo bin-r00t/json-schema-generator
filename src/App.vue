2f4476386d385e9255f10fc0f05b5966ef9d1eb9
liubin <liubin@liubin.website>
2025-02-25 13:27:38 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ const base = ref(20000);
 const saved = ref(6500);
 const target = ref(0);
 const months = ref(12);
+const winRate = ref(0.03);
 
 class DatePoint {
   constructor(year, month) {
@@ -23,11 +24,11 @@ class DatePoint {
   <div class="app-page grid place-content-center">
     <div class="border p-5 flex flex-col gap-3">
       <div class="form flex flex-col gap-3">
-        <label for="target">Target</label>
+        <label for="winRate">年化收益率</label>
         <input
           type="text"
-          id="target"
-          v-model="target"
+          id="winRate"
+          v-model="winRate"
           class="bg-gray-100 w-96 rounded p-1 px-4 appearance-none outline-none"
         />
       </div>
@@ -67,6 +68,9 @@ class DatePoint {
         <div v-for="n in +months" :key="n" class="flex items-center gap-3">
           <span class="w-18">第 {{ n }} 月: </span>
           <span class="flex-1">{{ +base + n * +saved }}</span>
+          <!-- <span class="flex-1">{{
+            ((+base + n * +saved) * (1 + winRate)).toFixed(2)
+          }}</span> -->
           <span class="w-24">{{ new DatePoint(2025, 1 + n).str }}</span>
         </div>
       </div>
